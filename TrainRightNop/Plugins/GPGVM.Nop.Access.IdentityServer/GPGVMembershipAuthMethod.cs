1a7f4225d3df71359111802216f141f15d5feb45
ramjet69 <nfs_john@hotmail.com>
2017-03-06 08:08:56 -0900

    Initial commit OpenId authorization / authentication NOP Plugin

---
+++ TrainRightNop/Plugins/GPGVM.Nop.Access.IdentityServer/GPGVMembershipAuthMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Routing;
+using Nop.Core.Plugins;
+using Nop.Services.Authentication.External;
+
+namespace GPGVM.Nop.Access.IdentityServer
+{
+    public class GPGVMembershipAuthMethod : BasePlugin, IExternalAuthenticationMethod
+    {
+        public void GetConfigurationRoute(out string actionName, out string controllerName, out RouteValueDictionary routeValues)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetPublicInfoRoute(out string actionName, out string controllerName, out RouteValueDictionary routeValues)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
